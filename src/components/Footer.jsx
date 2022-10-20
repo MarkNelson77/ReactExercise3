10816a33cc3e72f7e312f0887ad687b6810acfc2
MarkNelson77 <116048697+MarkNelson77@users.noreply.github.com>
2022-10-20 16:41:00 +0800

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
 
+const Footer = () => {
+  return (
+    <div>
+        <br /><br /><br /><br /><br />
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5">
+                <p class="m-0 text-center text-white">
+                    Copyright &copy; ReactJS Website 2022
+                </p>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
